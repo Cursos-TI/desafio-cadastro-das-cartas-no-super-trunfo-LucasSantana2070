7b932902196e44d0623733150cc5f599ea021e38
LucasSantana2070 <lucasqts2007@gmail.com>
2025-02-05 19:11:15 +0000

    criei um modelo de super trunfo novato.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char escolha;
+    while (1) {
+        printf("Escolha entre A á H: ");
+        scanf(" %c", &escolha);
+        if (escolha >= 'A' && escolha <= 'H') {
+            printf("Você escolheu a opção: %c\n", escolha);
+            break;
+        } else {
+            printf("Escolha inválida! Tente novamente.\n");
+        }
+    }
 
-    return 0;
+    int C_num;
+    int populacao;
+    float area;
+    float PIB;
+    int pontos_turisticos;
+    char nome[60];
+
+    while (1) {
+    printf("Digite um numero (1 - 4):");
+    scanf(" %d", &C_num);
+    if (C_num >= 1 && C_num <= 4){
+         printf("Você escolheu a cidade número: %d\n", C_num);
+            break;
+    } else { 
+        printf (" Escolha invalida! Tente novamente.\n");
+    }
+    }
+
+printf("Informe os dados da cidade abaixo.\n");
+printf("Qual o nome da cidade (Use _ para espaço): ");
+scanf(" %s", &nome);
+printf("População: ");
+scanf("%d", &populacao);
+printf("Área (em km²): ");
+scanf("%f", &area);
+printf("PIB (em R$): ");
+scanf("%f", &PIB);
+printf("Pontos turísticos: ");
+scanf("%d", &pontos_turisticos);
+
+return 0;
 }
+
